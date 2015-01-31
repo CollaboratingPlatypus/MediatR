5a7e7d86bae30addfa50a3a59362476befe91a42
Wade Baglin <plebmail@gmail.com>
2015-01-31 17:23:32 +1000

    Updates the nuspec file and build script

--- src/Portable.MediatR/Properties/AssemblyInfo.cs
+++ src/Portable.MediatR/Properties/AssemblyInfo.cs
@@ -27,4 +27,5 @@ using System.Resources;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
 [assembly: CLSCompliant(true)]
